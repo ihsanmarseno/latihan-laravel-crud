ce55591916600cc418ee618c50e57b276b563110
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2023-06-03 12:31:56 +0700

    first commit banh

---
+++ app/Http/Controllers/mahasiswaController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Session;
+use App\Models\mahasiswa;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;                        
+
+class mahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $katakunci = $request->katakunci;
+        $jumlahbaris = 4;
+        if (strlen($katakunci)) {
+            $data = mahasiswa::where('nim', 'like', "%$katakunci%")
+                ->orWhere('nama', 'like', "%$katakunci%")
+                ->orWhere('jurusan', 'like', "%$katakunci%")
+                ->paginate($jumlahbaris);
+        } else {
+            $data = mahasiswa::orderBy('nim', 'desc')->paginate($jumlahbaris);
+        }
+
+        return view('mahasiswa.index')->with('data', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(): View
+    {
+        return view('mahasiswa.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) : RedirectResponse
+    {
+        Session::flash('nim', $request -> nim);
+        Session::flash('nama', $request -> nama);
+        Session::flash('jurusan', $request -> jurusan);
+
+        $request->validate([
+            'nim' => 'required|numeric|unique:mahasiswa,nim',
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nim.required'=> 'NIM Wajib diisi',
+            'nim.numeric'=> 'NIM Wajib dalam angka',
+            'nim.unique'=> 'NIM sudah ada di dalam databse',
+            'nama.required'=> 'Nama Wajib diisi',
+            'jurusan.required'=> 'Jurusan Wajib diisi',
+        ]);
+        $data = [
+            'nim' => $request->nim,
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+
+        mahasiswa::create($data);
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil menambahkan data');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    
+    // public function show($id)
+    // {
+    //     //
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = mahasiswa::where('nim', $id)->first();
+        return view('mahasiswa.edit')->with('data', $data); 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) : RedirectResponse
+    {
+        $request->validate([
+            'nama' => 'required',
+            'jurusan' => 'required',
+        ], [
+            'nama.required'=> 'Nama Wajib diisi',
+            'jurusan.required'=> 'Jurusan Wajib diisi',
+        ]);
+        $data = [
+            'nama' => $request->nama,
+            'jurusan' => $request->jurusan,
+        ];
+
+        mahasiswa::where('nim', $id)->update($data);
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil melakukan update data');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) : RedirectResponse
+    {
+        mahasiswa::where('nim', $id)->delete();
+        return redirect()->to('mahasiswa')->with('success', 'Berhasil melakukan delete data');
+    }
+}
---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layout.template')
+
+@section('konten')
+<!-- START DATA -->
+<div class="my-3 p-3 bg-body rounded shadow-sm">
+        <!-- FORM PENCARIAN -->
+        <div class="pb-3">
+            <form class="d-flex" action="{{ url ('mahasiswa')}}" method="get">
+                <input class="form-control me-1" type="search" name="katakunci" value="{{ Request::get('katakunci') }}" placeholder="Masukkan kata kunci" aria-label="Search">
+                <button class="btn btn-secondary" type="submit">Cari</button>
+            </form>
+        </div>
+        
+        <!-- TOMBOL TAMBAH DATA -->
+        <div class="pb-3">
+            <a href='{{ url('mahasiswa/create')}}' class="btn btn-primary">+ Tambah Data</a>
+        </div>
+    
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th class="col-md-1">No</th>
+                    <th class="col-md-3">NIM</th>
+                    <th class="col-md-4">Nama</th>
+                    <th class="col-md-2">Jurusan</th>
+                    <th class="col-md-2">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = $data->firstItem() @endphp
+                @foreach ($data as $item)
+                    
+                <tr>
+                    <td>{{ $i }}</td>
+                    <td>{{ $item->nim}}</td>
+                    <td>{{ $item->nama}}</td>
+                    <td>{{ $item->jurusan}}</td>
+                    <td>
+                        <a href='{{ url('mahasiswa/' .$item->nim. '/edit') }}' class="btn btn-warning btn-sm">Edit</a>
+                        <form onsubmit="return confirm ('Yakin akan menghapus data?')" class="d-inline" action="{{ url('mahasiswa/' .$item->nim) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" name="submit" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+
+                @php $i++ @endphp
+                @endforeach
+            </tbody>
+        </table>
+        {{ $data->withQueryString()->links() }}
+    </div>
+    <!-- AKHIR DATA -->
+    
+@endsection
